0f92ea1c5d629365caab974636af852f3728c4ae
lex <savchenko.lex@gmail.com>
2023-12-04 04:48:46 +0200

    main section. install yup and formik

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -1,11 +1,26 @@
+import Image from 'next/image';
 import css from './Main.module.css';
+import arrrowRight from '../../public/arrow-right.svg';
 
 export default function Main (props) {
 
     return (<>
-        <div>
+        <div className={css.mainSection}>
             <div className="container">
-             <p>this is a main</p>
+            <div className={css.mainBox}></div>
+              <h1 className={css.mainHeader}>RENEWABLE ENERGY For any task</h1>
+              <div className={css.mainBoxText}>
+                <p className={css.mainDescription}>Development and implementation of renewable non-polluting energy sources, generating power generation using energy wind, sun, water, biomass</p>
+                <a href='#cases' className={css.learnMoreBtn} >
+                    <span>Learn more</span>
+                    <Image 
+                    src={arrrowRight}
+                    alt='Learn more button'
+                    width={16}
+                    height={16}
+                    />
+                </a>
+              </div>   
             </div>
         </div>
     </>)
