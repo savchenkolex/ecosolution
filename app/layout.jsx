c432b1a31462b60de2f6f14866ecfc01cfe82c90
lex <savchenko.lex@gmail.com>
2023-12-04 23:43:20 +0200

    fix Oswald font

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,11 +7,11 @@ export const FiraFont = Fira_Sans({
     variable: '--font-fira-sans',
 })
 
-export const OswaldFont = Oswald({
-    subsets: ['latin'],
-    weight: '400',
-    variable: '--font-oswald',
-})
+// export const OswaldFont = Oswald({
+//     subsets: ['latin-ext'],
+//     weight: ['400', '700'],
+//     variable: '--font-oswald',
+// })
 
 
 export default function RootLayout({ children }) {
@@ -19,6 +19,11 @@ export default function RootLayout({ children }) {
       <html lang="en">
         <head>
         <link rel="icon" href="ecosolution/favicon.png" sizes="any" />
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap" rel="stylesheet" />
+
         </head>
         <body>{children}</body>
       </html>
