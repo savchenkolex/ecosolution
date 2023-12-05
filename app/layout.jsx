8e3324db94cadbc2849a37e9ccb041da15f2feb0
lex <savchenko.lex@gmail.com>
2023-12-05 03:52:23 +0200

    another fix adding google fonts to project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,40 +1,26 @@
 import "./global.css";
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;500;700&family=Oswald:wght@400;700&display=swap');
-</style>
 
-// import { Fira_Sans, Oswald } from "next/font/google";
+import { Fira_Sans, Oswald } from "next/font/google";
 
-// export const FiraFont = Fira_Sans({
-//   subsets: ["latin"],
-//   weight: "400",
-//   variable: "--font-fira-sans",
-// });
+export const FiraFont = Fira_Sans({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-fira",
+});
 
-// export const OswaldFont = Oswald({
-//     subsets: ['latin-ext'],
-//     weight: ['400', '700'],
-//     variable: '--font-oswald',
-// })
+export const OswaldFont = Oswald({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--font-oswald',
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${FiraFont.variable} ${OswaldFont.variable}`}>
       <head>
         <link rel="icon" href="ecosolution/favicon.png" sizes="any" />
 
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap"
-          rel="stylesheet"
-        /> */}
+        
       </head>
       <body>{children}</body>
     </html>
