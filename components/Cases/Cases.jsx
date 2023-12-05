8f626c51c910c245920717a5f62dbb0d3053b62d
lex <savchenko.lex@gmail.com>
2023-12-05 05:13:14 +0200

    add buttons with arrows in cases

--- components/Cases/Cases.jsx
+++ components/Cases/Cases.jsx
@@ -3,13 +3,36 @@ import css from "./Cases.module.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import {Navigation, Pagination} from 'swiper/modules';
 import "swiper/css";
+import Image from "next/image";
+import arrow from "../../assets/icons/arrow.svg";
 
 export default function Cases(props) {
   return (
     <>
-      <div id="cases">
+      <div id="sectioncases" className={css.sectionCases}>
         <div className="container">
-          <h2 className="section-header">Successful cases of our company</h2>
+          <h2 className="section-header">
+            Successful cases of our company
+          </h2>
+          <div className={css.controllBox}>
+            <p><span>01</span>/<span>05</span></p>
+            <div className={css.buttonBox}>
+            <button className={css.leftBtn}>
+                <Image  
+                src={arrow}
+                alt="left button with arrow"
+                className={css.leftArrow}
+                />
+            </button>
+            <button className={css.rightBtn}>
+                <Image  
+                src={arrow}
+                alt="right button with arrow"
+                className={css.rightArrow}
+                />
+            </button>
+            </div>
+          </div>
           <Swiper
             spaceBetween={50}
             slidesPerView={3}
