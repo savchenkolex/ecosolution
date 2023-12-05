763292179dd7e71b51d3f6ac7d2d440676a137bc
lex <savchenko.lex@gmail.com>
2023-12-05 05:25:36 +0200

    fix some style for desktop

--- components/Electricity/Electricity.jsx
+++ components/Electricity/Electricity.jsx
@@ -4,17 +4,19 @@ export default function Electricity (props) {
 
     return (<>
         <div className={css.electricitySection}>
-            <div className="container">
-                <h2 className='section-header'>
-                    Electricity we produced for all time
-                </h2>
-                <div className={css.decorationBox}>
-                    <div className={css.decorationLine}></div>
+            <div className={css.electricityBox}>
+                <div className="container">
+                    <h2 className='section-header'>
+                        Electricity we produced for all time
+                    </h2>
+                    <div className={css.decorationBox}>
+                        <div className={css.decorationLine}></div>
+                    </div>
+                    <p className={css.electricityCounter}>
+                        <span>1.134.147.814</span> 
+                        <span className={css.kWh}>kWh</span>
+                    </p>
                 </div>
-                <p className={css.electricityCounter}>
-                    <span>1.134.147.814</span> 
-                    <span className={css.kWh}>kWh</span>
-                </p>
             </div>
         </div>
     </>)
